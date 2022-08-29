c7959d1966b8d09b6bc32033d49b234839abf785
developerlenin <justlenin9095@gmail.com>
2022-08-29 14:20:05 +0530

    new

---
+++ javascript.js
@@ -0,0 +1,83 @@
+/*
+  var num = 1+1+'volvo'
+  var num1 = 'volvo'+1+1
+  console.log(num1)
+  
+  const car =["Saab", "Volvo", "BMW"];
+  
+  const cars = [0];
+cars[0]= "Saab";
+cars[1]= "Volvo";
+cars[2]= "BMW";
+  console.log(car)
+  console.log(cars)
+  var name12;
+  console.log(name12);
+  
+  var name13
+   console.log(name13);
+  */
+  //object//
+
+   preson = {
+	   name: "lenin",
+	   age: 24,
+	   address: "tenkasi",   
+   } 
+    preson.age = 26
+    
+	 console.log(preson)
+	 console.log(typeof preson)
+ 
+   //array 
+   var favcolor = [ "red","green","black"]
+    favcolor [4] = "yellow" 
+	favcolor [7] = null 
+	favcolor[0]="yellow";
+	
+   console.log(favcolor)
+    console.log(typeof favcolor)
+	{
+	let name = "lenin"
+	let msg = "hi"+ name + "hello"
+	 console.log(msg)
+	}
+	{
+	function welcome(num1,num2) {
+	console.log(num1*num2)
+	}
+	welcome(3,6)
+	}
+	{
+	function welcome (name){
+		
+		let msg = "hi" + name + "Welcome to java";
+		
+		console . log(msg)
+	}
+	
+	
+	  welcome("lenin" )
+	  welcome("cs" )
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+   
+  
+  
+  
+  
+  
